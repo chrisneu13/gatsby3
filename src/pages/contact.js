d479ddd440fe028bdb42b6ca58c6e33fd0a21d7e
chrisneu13 <christian.neubauer@mailbox.org>
2021-03-10 18:43:38 +0100

    noch ein Versuch zu deployen

---
+++ src/pages/contact.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function Contact() {
+  return (
+      <Layout>
+    <div>
+      <h1>Wer mich kontakten will, koennte hier eine E-Mail schreiben.</h1>
+      <p>
+        <a href="mailto:me@example.com">me@example.com</a>
+      </p>
+    </div>
+    </Layout>
+  )
+}
