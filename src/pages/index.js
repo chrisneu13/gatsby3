b223a8db6c135b710345f87d84a18452310472ed
chrisneu13 <christian.neubauer@mailbox.org>
2021-03-10 18:39:26 +0100

    Nur die about.js fuer demo geaendert

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,18 @@
 import React from "react"
+import Layout from "../components/layout"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <h1>Hi Chris!!! I'm building a fake Gatsby site as part of a tutorial!</h1>
+      <p>
+        What do I like to do? Lots of course but definitely enjoy building
+        websites.
+      </p>
+      <div>
+      <Link to="/contact/">Contact</Link>
+    </div>
+    </Layout>
+  );
 }
