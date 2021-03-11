5930a7121ca9f44322e024aa7c13dd1ab779106f
chrisneu13 <christian.neubauer@mailbox.org>
2021-03-11 14:47:26 +0100

    noch was neues

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,7 +5,7 @@ export default function Contact() {
   return (
       <Layout>
     <div>
-      <h1>Wer mich kontakten will, koennte hier eine E-Mail schreiben.</h1>
+      <h1>Wer mich kontakten will, koennte hier eine E-Mail schreiben und nochmal geaendert.</h1>
       <p>
         <a href="mailto:me@example.com">me@example.com</a>
       </p>
