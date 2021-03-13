93989cfb8eb8a902c991f2d4211c2ec6793fcc85
chrisneu13 <christian.neubauer@mailbox.org>
2021-03-13 19:38:42 +0100

    jetzt mit static Bild

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,5 +7,16 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
 import React from "react"
 import Layout from "../components/layout"
 import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+
 
 export default function Home() {
   return (
@@ -10,6 +13,18 @@ export default function Home() {
         Ach ja Umlaute müssten auch funktionieren.
       </p>
       <p>und noch ein Absatz</p>
+      <p>Wo ist nun das Bild? Hier wohl nicht?</p>
+
+
+      <StaticImage
+      src="../images/rotesauto.jpg"
+      alt="A dinosaur"
+      placeholder="blurred"
+      layout="fixed"
+      width={200}
+      height={200}
+    />
+     
       <div>
       <Link to="/contact/">Contact</Link>
     </div>
