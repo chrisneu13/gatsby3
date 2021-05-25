2323a9ae345f8409f536452f9c72a74217bca34e
chrisneu13 <christian.neubauer@mailbox.org>
2021-05-25 20:21:00 +0200

    mit Wetterdaten

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,10 +6,9 @@ export default function About() {
   return (
       <Layout>
     <div>
-      <h1>Alles über mich</h1>
-      <p>
-        Das was ich immer mal über mich sagen wollte.
-      </p>
+      <h1>Aktuelles Wetter</h1>
+       <div><iframe width="400px" height="500px" title="3 Tage Trend" src="https://morgenwirdes.de/api/v2/3day.php?plz=90610"
+       frameborder="0" scrolling="no"></iframe></div>
       <img src={blume} alt="Logo" />
     </div>
     </Layout>
